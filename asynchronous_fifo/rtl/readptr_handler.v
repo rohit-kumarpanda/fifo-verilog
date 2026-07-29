a5110d056ed1387d7c374dd96aea28ba62906afa
rohit-kumarpanda <rohitarts14@gmail.com>
2026-07-29 18:20:15 +0530

    Refactor module header in readptr_handler.v
    Updated module header with author and description details.

--- asynchronous_fifo/rtl/readptr_handler.v
+++ asynchronous_fifo/rtl/readptr_handler.v
@@ -1,25 +1,17 @@
+//////////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ * Module Name : readptr_handler
+ * Author      : Rohit Kumar Panda
+ * Date        : 29-Jul-2026
+ *
+ * Description :
+ * Generates the binary and Gray-code read pointers.
+ * Detects FIFO empty condition using synchronized Gray-code write pointer.
+ *
+ ******************************************************************************/
+//////////////////////////////////////////////////////////////////////////////////
+
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.07.2026 22:02:31
-// Design Name: 
-// Module Name: readptr_handler
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module readptr_handler #(parameter ADDRESS_WIDTH = 3)(
     input clk ,
     input rrst_n ,
@@ -34,12 +26,17 @@ wire rempty;
 wire [ADDRESS_WIDTH:0] b_rptr_nxt;
 wire [ADDRESS_WIDTH:0] g_rptr_nxt;
 
+// Next-state pointer calculation
 assign b_rptr_nxt = (b_rptr) + (r_en & ~empty);
 assign g_rptr_nxt = (b_rptr_nxt >> 1 ) ^ b_rptr_nxt;
+
+// Empty flag generation
 assign rempty = (g_rptr_nxt == g_wptr_sync);
 
 //Register
 always @(posedge clk or negedge rrst_n) begin
+    
+    // Asynchronous active-low reset
     if(!rrst_n) begin
         b_rptr <= 0;
         g_rptr <= 0;
